4d9721f8bfdccb0dc57da76b767b175160fa0cf5
Anush79 <barbieworld1056.a@gmail.com>
2023-07-09 21:58:06 +0530

    country destinations

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -1,18 +1,24 @@
-
-import Card from '../components/Card'
-import {data} from '../data'
-import { NavLink, useParams } from 'react-router-dom'
-export default function Countries(){
-const {continent}= useParams()
-  const continentFound= data.continents.find(item=>item.name.includes(continent))
-  console.log(continentFound)
-  return<><h3 className="m-10 text-2xl font-bold text-blue-500">Top continents for your next holiday</h3>
-  <div className='flex flex-row gap-4 relative'>
-  {
-      continentFound?.countries?.map(item=><NavLink to={`/${item.name.slice(0,4)}/${item.name.slice(0,4)}`}><Card item={item}/>
-        </NavLink>)
-    }
-  
-  </div>
-  </>
+import Card from "../components/Card";
+import { data } from "../data";
+import { NavLink, useParams } from "react-router-dom";
+export default function Countries() {
+  const { continent } = useParams();
+  const continentFound = data.continents.find((item) =>
+    item.name.includes(continent)
+  );
+  console.log(continentFound);
+  return (
+    <>
+      <h3 className="m-10 text-2xl font-bold text-blue-500">
+        Top continents for your next holiday
+      </h3>
+      <div className="flex flex-row gap-4 relative">
+        {continentFound?.countries?.map((item) => (
+          <NavLink to={`/${item.name.slice(0, 4)}/${item.name.slice(0, 4)}`}>
+            <Card item={item} />
+          </NavLink>
+        ))}
+      </div>
+    </>
+  );
 }
--- src/pages/Places.jsx
+++ src/pages/Places.jsx
@@ -1,9 +1,24 @@
-
+import { useParams } from 'react-router-dom';
+import {data} from '../data'
 import Card from '../components/Card'
-export default function Countries({destinations}){
-  return <div>
-    {
-      destinations?.map(item=><Card item={item}/>)
-    }
-  </div>
+export default function Countries(){
+  const { continent, country } = useParams();
+  const continentFound = data.continents.find((item) =>
+    item.name.includes(continent)
+  );
+
+  const countryFound = continentFound.find(item=>  item.name.includes(country))
+  return (
+    <>
+      <h3 className="m-10 text-2xl font-bold text-blue-500">
+        Top destinations for your next holiday in {countryFound}
+      </h3>
+      <div className="flex flex-row gap-4 relative">
+        {countryFound?.destinations?.map((item) => (
+          <NavLink to={`/${item.name.slice(0, 4)}/${item.name.slice(0, 4)}`}>
+            <Card item={item} />
+          </NavLink>
+        ))}
+      </div>
+    </>)
 }
